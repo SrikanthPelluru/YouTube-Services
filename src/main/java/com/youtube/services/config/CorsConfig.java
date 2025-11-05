82212535e3856e2b94503b260f244f2b85065e9d
SrikanthPelluru <137858219+SrikanthPelluru@users.noreply.github.com>
2025-11-05 12:37:33 +0530

    Updated config

---
+++ src/main/java/com/youtube/services/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.youtube.services.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        // TODO: Replace with your frontend URLs
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://ad-free-youtube.onrender.com/"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
